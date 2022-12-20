b93f72edc63523ab3528f507fa8879bae211e210
Evgenii Troinov <troinof@gmail.com>
2022-12-20 11:04:19 +0100

    feat(init): prepare for markup

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn">test</button>
+  <button class="btn-default">test</button>
+  <button class="btn-primary">test</button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.btn {
+
+}
+</style>
