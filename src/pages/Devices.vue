deff2d28904bdc5823dbebae2031947cd95c1ea0
Evgenii Troinov <troinof@gmail.com>
2023-01-04 20:54:24 +0100

    feat(master): update links

--- src/pages/Devices.vue
+++ src/pages/Devices.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div class="container">
+    <header class="page-header">
+      <Typography>Devices</Typography>
+    </header>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Typography from '@/components/Typography.vue';
+</script>
