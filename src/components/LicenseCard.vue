cabc0bc4318c8aff7ca7eecc056f5c0a2fc957f4
Evgenii Troinov <troinof@gmail.com>
2023-01-11 16:34:36 +0100

    feat(main): add license card

---
+++ src/components/LicenseCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="component">
+    <header class="component__header">
+      <h2 class="component__title">Basic Plan</h2>
+      <h3 class="component__subTitle">
+        The essentials for a budding buisness: update and manage your
+        information
+      </h3>
+    </header>
+    <hr class="component__devider" />
+
+    <div class="component__body">
+      <h4>Features</h4>
+      <ul class="list">
+        <li class="list__item">
+          <i class="ico icon-check"></i>
+          <span class="list__text">RSS based push</span>
+        </li>
+        <li class="list__item">
+          <i class="ico icon-check"></i>
+          <span class="list__text">Notification</span>
+        </li>
+        <li class="list__item">
+          <i class="ico icon-check"></i>
+          <span class="list__text">Plugin access settings</span>
+        </li>
+        <li class="list__item">
+          <i class="ico icon-check"></i>
+          <span class="list__text">GEO fence push</span>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.component {
+}
+</style>
--- src/pages/Licenses.vue
+++ src/pages/Licenses.vue
@@ -3,9 +3,16 @@
     <header class="page-header">
       <Typography>Licenses</Typography>
     </header>
+
+    <div>
+      <LicenseCard />
+      <LicenseCard />
+      <LicenseCard />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Typography from '@/components/Typography.vue';
+import LicenseCard from '@/components/LicenseCard.vue';
 </script>
