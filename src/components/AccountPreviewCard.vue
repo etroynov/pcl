01cdba926823467ead1c59d1b21500cd92a63660
Evgenii Troinov <troinof@gmail.com>
2023-01-17 01:09:34 +0100

    feat(master): add account card components

---
+++ src/components/AccountPreviewCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="component">
+    <div class="component__col">
+      <img src="" alt="" />
+      <div class="component__name"></div>
+    </div>
+    <div class="component__col">
+      <a href="" class="component__link">connect</a>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/Clients.vue
+++ src/pages/Clients.vue
@@ -3,9 +3,14 @@
     <header class="page-header">
       <Typography>Clients</Typography>
     </header>
+
+    <div>
+      <AccountCard />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Typography from '@/components/Typography.vue';
+import AccountCard from '@/components/AccountCard.vue';
 </script>
