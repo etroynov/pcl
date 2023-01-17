ec404a760bbd46b2c58e009d03c7fba59825cc14
Evgenii Troinov <troinof@gmail.com>
2023-01-17 01:13:56 +0100

    feat(master): add subscription card components

---
+++ src/components/SubscriptionsSettingsCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component">
+    <header class="component__header">
+      <h3 class="component__title">Special sales</h3>
+    </header>
+
+    <div class="component__body">
+      <div class="property">
+        <div class="property__name">email</div>
+        <div class="property__control"></div>
+      </div>
+
+      <div class="property">
+        <div class="property__name">email</div>
+        <div class="property__control"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
